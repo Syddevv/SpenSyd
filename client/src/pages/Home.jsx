9ac4dbffa0b62870d9454ee00d1454f3a8aad3d7
Syddev <sydneysantos176@gmail.com>
2025-07-13 18:56:29 +0800

    moved quick access to home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "../components/NavBar";
 import ExpenseStat from "../components/ExpenseStat";
 import IncomeStat from "../components/IncomeStat";
 import "../styles/HomePage.css";
 import RecentActWrapper from "../components/RecentActWrapper";
-import QuickAccess from "../components/QuickAccess";
 import Chart from "../components/Chart";
+import Modal from "../components/Modal";
+import newExpenseIcon from "../assets/add expenses icon.png";
+import addBalanceIcon from "../assets/add income icon.png";
+import "../styles/QuickAccess.css";
 
 const Home = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <div className="homePage">
       <NavBar />
@@ -21,7 +25,26 @@ const Home = () => {
 
       <div className="recent_Controls">
         <RecentActWrapper />
-        <QuickAccess />
+
+        <div className="controlsContainer">
+          <div className="controlsWrapper">
+            <div className="newExpense">
+              <div>
+                <img src={newExpenseIcon} className="quickAccessIcon" />
+              </div>
+              <p className="label">+ New Expense</p>
+            </div>
+
+            <div className="addBalance">
+              <div>
+                <img src={addBalanceIcon} className="quickAccessIcon" />
+              </div>
+              <p className="label">+ Balance</p>
+            </div>
+          </div>
+
+          <p className="groupLabel">Quick Access</p>
+        </div>
       </div>
 
       <div className="chart">
