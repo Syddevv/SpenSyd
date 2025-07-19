3db02e41d242242ecb8e2d447ef3ec23911c82c7
Syddev <sydneysantos176@gmail.com>
2025-07-19 20:52:14 +0800

    adding balance

--- server/controllers/expensesController.js
+++ server/controllers/expensesController.js
@@ -10,12 +10,12 @@ export const addExpense = async (req, res) => {
         .json({ success: false, message: "Expense is Required" });
     }
 
-    const newExpense = new ExpenseModel({
+    const newBalance = new ExpenseModel({
       ...expense,
       userId: req.user.id,
     });
 
-    await newExpense.save();
+    await newBalance.save();
 
     return res.status(201).json({
       success: true,
--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ import cors from "cors";
 import dotenv from "dotenv";
 import authRouter from "./routes/authRoutes.js";
 import expenseRouter from "./routes/expensesRoutes.js";
-import incomeRouter from "./routes/incomesRoutes.js";
 
 dotenv.config();
 
@@ -16,7 +15,6 @@ app.use(express.json());
 
 app.use("/api/auth", authRouter);
 app.use("/api/expense", expenseRouter);
-app.use("/api/balance", incomeRouter);
 
 app.listen(PORT, () => {
   connectToDb();
